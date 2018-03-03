b25631691a99cf403402e7aef239a1211018c42c
Justin Gaudet <c041u@unb.ca>
2018-03-03 15:26:02 -0400

    Created Settings UI and added functionality to launch

--- src/AssignSubstitutes/Controller.java
+++ src/AssignSubstitutes/Controller.java
@@ -1,5 +1,31 @@
 package AssignSubstitutes;
 
+import AssignSubstitutes.Settings.SettingsController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
-    public Controller(){}
+    @FXML
+    private void clickSettings() throws IOException{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Settings/SettingsUI.fxml"));
+            Parent root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            //stage.initModality(Modality.APPLICATION_MODAL);
+            //stage.initStyle(StageStyle.UNDECORATED);
+            //stage.setScene(new Scene(root1, 200, 150));
+            stage.setTitle("Settings");
+            stage.setScene(new Scene(root1));
+
+            //get the controller and send it the stage
+            SettingsController controller = fxmlLoader.getController();
+            controller.setStage(stage);
+
+            //show
+            stage.show();
+    }
 }

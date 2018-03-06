91cdce67686cd6e36d3c75e082000a4ce830997f
OriskaW <36309268+OriskaW@users.noreply.github.com>
2018-03-05 21:20:02 -0400

    Added Files

---
+++ Structure-Oncall/Period.java
@@ -0,0 +1,16 @@
+public class period{
+	String course;
+	String teachable;
+	int roomNumber;
+	int periodNumber;
+	boolean absent=false;
+	public period(boolean isAbsent){
+		absent=true;
+	}
+	public period()
+	public String getCoure(){return course;}
+	public String getTeachable(){return teachable;}
+	public int getRoomNumber(){return roomNumber;}
+	public int getPeriodNumber(){return periodNumber;}
+	public boolean Absent(){return isAbsent;}
+}

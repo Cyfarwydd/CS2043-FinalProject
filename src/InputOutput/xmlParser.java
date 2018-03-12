ce7e21423c7ed8bac5b62aa8d7641b13e4cd259f
Josh Watson <jwatson9@unb.ca>
2018-03-12 04:50:33 -0300

    Added writing to the XML file along with setter methods.

--- src/InputOutput/xmlParser.java
+++ src/InputOutput/xmlParser.java
@@ -2,12 +2,19 @@ package InputOutput;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.IOException;
 
 public class xmlParser {
@@ -25,7 +32,7 @@ public class xmlParser {
 
         try
         { doc = getDocument(filepath); }
-        catch (SAXException | ParserConfigurationException | IOException e)
+        catch (TransformerException | SAXException | ParserConfigurationException | IOException e)
         { e.printStackTrace(); }
 
         NodeList config = doc.getElementsByTagName("config");
@@ -66,43 +73,43 @@ public class xmlParser {
         return defaultCoverageView;
     }
 
-    public void setMasterResetDate(String masterResetDate) throws ParserConfigurationException, SAXException, IOException
+    public void setMasterResetDate(String masterResetDate) throws ParserConfigurationException, SAXException, IOException, TransformerException
     {
-        writeXML(masterResetDate, "config", "masterResetDate");
+        writeXML(masterResetDate, "masterResetDate");
         this.masterResetDate = masterResetDate;
     }
 
-    public void setMaxWeeklyTally(Integer maxWeeklyTally) throws ParserConfigurationException, SAXException, IOException
+    public void setMaxWeeklyTally(Integer maxWeeklyTally) throws ParserConfigurationException, SAXException, IOException, TransformerException
     {
-        writeXML(maxWeeklyTally.toString(), "config", "maxWeeklyTally");
+        writeXML(maxWeeklyTally.toString(), "weeklyMax");
         this.maxWeeklyTally = maxWeeklyTally;
     }
 
-    public void setMaxMonthlyTally(Integer maxMonthlyTally) throws ParserConfigurationException, SAXException, IOException
+    public void setMaxMonthlyTally(Integer maxMonthlyTally) throws ParserConfigurationException, SAXException, IOException, TransformerException
     {
-        writeXML(maxMonthlyTally.toString(), "config", "maxMonthlyTally");
+        writeXML(maxMonthlyTally.toString(),"monthlyMax");
         this.maxMonthlyTally = maxMonthlyTally;
     }
 
-    public void setTempWeeklyMax(Integer tempWeeklyMax) throws ParserConfigurationException, SAXException, IOException
+    public void setTempWeeklyMax(Integer tempWeeklyMax) throws ParserConfigurationException, SAXException, IOException, TransformerException
     {
-        writeXML(tempWeeklyMax.toString(), "config", "tempWeeklyMax");
+        writeXML(tempWeeklyMax.toString(), "tempWeeklyMax");
         this.tempWeeklyMax = tempWeeklyMax;
     }
 
-    public void setTempMonthlyMax(Integer tempMonthlyMax) throws ParserConfigurationException, SAXException, IOException
+    public void setTempMonthlyMax(Integer tempMonthlyMax) throws ParserConfigurationException, SAXException, IOException, TransformerException
     {
-        writeXML(tempMonthlyMax.toString(), "config", "tempMonthlyMax");
+        writeXML(tempMonthlyMax.toString(), "tempMonthlyMax");
         this.tempMonthlyMax = tempMonthlyMax;
     }
 
-    public void setDefaultCoverageView(Integer defaultCoverageView) throws ParserConfigurationException, SAXException, IOException
+    public void setDefaultCoverageView(Integer defaultCoverageView) throws ParserConfigurationException, SAXException, IOException, TransformerException
     {
-        writeXML(defaultCoverageView.toString(), "config", "defaultCoverageView");
+        writeXML(defaultCoverageView.toString(), "defaultCoverageView");
         this.defaultCoverageView = defaultCoverageView;
     }
 
-    private static Document getDocument(String filePath) throws ParserConfigurationException, SAXException, IOException
+    private static Document getDocument(String filePath) throws ParserConfigurationException, SAXException, IOException, TransformerException
     {
         DocumentBuilderFactory docBF = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = docBF.newDocumentBuilder();
@@ -111,11 +118,18 @@ public class xmlParser {
         return doc;
     }
 
-    private void writeXML(String input, String rootElement, String childElement)throws ParserConfigurationException, SAXException, IOException
+    private void writeXML(String input, String childElement) throws ParserConfigurationException, SAXException, IOException, TransformerException
     {
         Document doc = getDocument(filepath);
-        NodeList root = doc.getElementsByTagName(rootElement);
-        Element out = (Element) root.item(0);
-        out.getElementsByTagName(childElement).item(0).setTextContent(input);
+        Node out = doc.getElementsByTagName(childElement).item(0);
+        out.setTextContent(input);
+
+
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new File(filepath));
+        transformer.transform(source, result);
+
     }
 } // class
